92e84f9a622c2a086b18762a2d1a86e8280bc646
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-01-11 20:16:42 -0300

    Início do projeto. Criação do Navbar.

---
+++ assets/js/header.js
@@ -0,0 +1,1 @@
+document.getElementById("cabecalho").style.backgroundColor = "pink"
---
+++ assets/js/transparent.js
@@ -0,0 +1,24 @@
+
+var nav = document.querySelector('nav');
+var li = document.getElementsByClassName("nav-item")
+
+var buttonCollapse = document.getElementById("botao-collapse")
+
+const buttonOptions = document.getElementById('navbarTogglerDemo02')
+
+buttonCollapse.addEventListener("click", function (){
+    console.log("Cliquei no botão")
+    buttonOptions.style.display = "show"
+})
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset < -2) {
+        console.log("Na cor Light")
+        nav.classList.add('bg-light', 'shadow', 'navbar-light');
+    } else {
+        console.log("Transparente")
+        nav.classList.remove('bg-light', 'shadow');
+        //nav.classList.add('navbar-light')
+        buttonOptions.classList.remove('show')
+    }
+});
