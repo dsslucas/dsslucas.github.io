f631976b9216d1125f6dccfd80b6da8cb8b6cb48
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-01-12 16:02:25 -0300

    Modificações na transparência. Adição de wallpaper.

--- assets/js/transparent.js
+++ assets/js/transparent.js
@@ -1,14 +1,32 @@
-
+//Elementos do Nav
 var nav = document.querySelector('nav');
 var li = document.getElementsByClassName("nav-item")
-
 var buttonCollapse = document.getElementById("botao-collapse")
-
 const buttonOptions = document.getElementById('navbarTogglerDemo02')
+
+// Elementos do Presentation
+var h1 = document.getElementById("h1-presentation")
+var p = document.getElementById("p-presentation")
+var a = document.getElementById("presentation").getElementsByTagName("a")
+
+//Botão de retorno à página inicial
+var buttonReturn = document.getElementById("return")
 
 window.addEventListener('load', function (){
     console.log("Carreguei!")
-    nav.classList.add('navbar-light');
+    //nav.classList.add('navbar-light')
+    nav.classList.add('navbar-dark')
+    h1.style.color = "white"
+    p.style.color = "white"
+
+    buttonReturn.style.display = "none"
+
+
+    for(let i=0; i< a.length; i++){
+        a[i].style.color = "white"
+    }
+
+    console.log(window)
 })
 
 buttonCollapse.addEventListener("click", function (){
@@ -17,13 +35,31 @@ buttonCollapse.addEventListener("click", function (){
 })
 
 window.addEventListener('scroll', function () {
-    if (window.pageYOffset < -2) {
-        console.log("Na cor Light")
-        nav.classList.add('bg-light', 'shadow', 'navbar-light');
-    } else {
+    //Contempla apenas a página inicial
+    console.log(window.pageYOffset)
+    if (window.pageYOffset <= 700) {
+        console.log("Na cor Dark")
+        nav.classList.add('navbar-dark');
+        nav.classList.remove("navbar-light")
+        buttonReturn.style.display = "none"
+    }
+    else{
+        console.log("Saí da página inicial")
+        nav.classList.remove("navbar-dark")
+        nav.classList.add("navbar-light")
+        buttonReturn.style.display = "inline"
+    }
+    /*
+    else if(window.pageYOffset > 700){
+        console.log("Saí da página inicial")
+        nav.classList.remove("navbar-dark")
+        nav.classList.add("navbar-light")
+    }
+    else {
         console.log("Transparente")
         nav.classList.remove('bg-light', 'shadow');
         //nav.classList.add('navbar-light')
         buttonOptions.classList.remove('show')
     }
+    */
 });
