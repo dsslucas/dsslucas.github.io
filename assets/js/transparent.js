529759ebc3cad7e47df6d7219af932cfc657e3f3
Lucas Silva <lucas_silva_souza@hotmail.com>
2022-01-31 17:25:16 -0300

    Alterações no CSS e efeitos no Nav para clique de botão

--- assets/js/transparent.js
+++ assets/js/transparent.js
@@ -7,39 +7,48 @@ const buttonOptions = document.getElementById('navbarTogglerDemo02')
 //Botão de retorno à página inicial
 var buttonReturn = document.getElementById("return")
 
-window.addEventListener('load', function (){
+window.addEventListener('load', function () {
     nav.classList.add('navbar-dark')
 
     buttonReturn.style.display = "none"
 
-    if(window.pageYOffset > 360){
+    if (window.pageYOffset > 360) {
         nav.classList.remove('navbar-dark')
         nav.classList.add('navbar-light')
         buttonReturn.style.display = "inline"
     }
 })
 
-buttonCollapse.addEventListener("click", function (){
+buttonCollapse.addEventListener("click", function () {
     buttonOptions.style.display = "show"
-    if(window.pageYOffset < 520){
+    if (window.pageYOffset < 520) {
         nav.classList.add('bg-dark')
     }
-    else{
+    else {
         nav.classList.add('bg-light')
+    }
+
+    if (buttonCollapse.classList.contains('collapsed')) {
+        console.log("Entrei na condição")
+
+        buttonOptions.classList.add('collapsing')
+        //Remove o fundo, que é criado quando há o clique no Nav
+        nav.classList.remove('bg-light')
+        nav.classList.remove('bg-dark')
     }
 })
 
 window.addEventListener('scroll', function () {
 
-    //Remove o fundo, que é criado quando há o clique no Nav
-    nav.classList.remove('bg-light')
-    nav.classList.remove('bg-dark')
+        //Remove o fundo, que é criado quando há o clique no Nav
+        nav.classList.remove('bg-light')
+        nav.classList.remove('bg-dark')
 
     //Remove o show do Nav
     buttonCollapse.classList.add('collapsed')
     buttonCollapse.ariaExpanded = false
     buttonOptions.classList.remove("show")
-    
+
     //Contempla apenas a página inicial
     if (window.pageYOffset < 520) {
         nav.classList.add('navbar-dark');
@@ -47,7 +56,7 @@ window.addEventListener('scroll', function () {
         buttonReturn.style.display = "none"
     }
 
-    else{
+    else {
         nav.classList.remove("navbar-dark")
         nav.classList.add("navbar-light")
         buttonReturn.style.display = "inline"
